5a5fa4f96d092aa45ce79ae5f48ee0f908f069b4
Cjsheaf <cjsheaf@cox.net>
2015-01-01 23:19:18 -0800

    Christopher Sheaf - Version 0.86
    Modified the Homology Modeling task to use "moebatch" with a GUI-less version of the SVL script.

--- Pipeline_Tasks/Homology_Modeling.py
+++ Pipeline_Tasks/Homology_Modeling.py
@@ -17,7 +17,12 @@ class HomologyModelingTask(Task):
     def parse_xml_parameters(self, xml_parameters):
         self.args['svl_script_name'] = path.basename(xml_parameters.find('svl_filepath').text)
         self.args['svl_directory'] = path.dirname(xml_parameters.find('svl_filepath').text)
-        self.args['input_directory'] = xml_parameters.find('input_directory').get('path')
+
+        input_directory = xml_parameters.find('input_directory')
+        in_path = input_directory.get('path')
+        self.args['homology_options'] = path.join(in_path, input_directory.find('homology_options').text)
+        self.args['template_file'] = path.join(in_path, input_directory.find('template_file').text)
+        self.args['sequence_file'] = path.join(in_path, input_directory.find('sequence_file').text)
 
     def run(self):
         # MOE needs to be invoked from the directory containing the SVL scripts, otherwise it can't seem to
@@ -33,8 +38,10 @@ class HomologyModelingTask(Task):
         #process_args = list(lex)
         #check_call(process_args, stdout=PIPE, cwd=self.args['svl_directory'])
 
-        process_args = 'moe -load "{script}" -exec "HomologyBatch [\'{input}\']"'.format(
+        process_args = 'moe -load "{script}" -exec "HomologyBatch_NoGUI [\'{options}\', \'{template}\', \'{sequence}\']"'.format(
             script=self.args['svl_script_name'],  # The file will be accessed from the parent dir.
-            input=posixpath.relpath(self.args['input_directory'], start=self.args['svl_directory'])
+            options=posixpath.relpath(self.args['input_directory'], start=self.args['homology_options']),
+            template=posixpath.relpath(self.args['input_directory'], start=self.args['template_file']),
+            sequence=posixpath.relpath(self.args['input_directory'], start=self.args['sequence_file'])
         )
         check_call(process_args, stdout=PIPE, shell=True, cwd=self.args['svl_directory'])
